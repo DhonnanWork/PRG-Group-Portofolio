87869676ae0f09c1b11a8f3e6fd7a3c8a92eaef6
unknown <Dhonnanwork@gmail.com>
2025-10-21 20:01:22 +0700

    Deploy Server

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/PRG-Group-Portofolio',
+  assetPrefix: '/PRG-Group-Portofolio/',
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'picsum.photos', // <-- CHANGE THIS LINE
+        hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
       },
